99c126c6d3f13cfc6ca5a49abb33a82b67324721
lucasezidro <lucasezidro7@gmail.com>
2023-01-19 23:07:11 -0300

    create modal and introduce radix-ui

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -12,11 +12,11 @@ export function SummaryTable() {
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
-        {weekDays.map((weekDay, index) => {
+        {weekDays.map((weekDay, i) => {
           return (
             <div>
               <div
-                key={`${weekDay}-${index}`}
+                key={`${weekDay}-${i}`}
                 className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
               >
                 {weekDay}
@@ -28,7 +28,13 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => {
-          return <HabitDay key={date.toString()} />;
+          return (
+            <HabitDay 
+              key={date.toString()} 
+              amount={5} 
+              completed={Math.round(Math.random() * 5)} 
+            />
+          )
         })}
 
         {amountOfDaysToFill > 0 &&
